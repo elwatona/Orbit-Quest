b86936a47baf3b5f6afa7a80964e999f12c70edd
elwatona <elwa.bazaesg@gmail.com>
2026-04-23 17:40:37 -0400

    chore: move scripts and folders

--- Assets/Scripts/IDebbugeable.cs
+++
@@ -1,8 +0,0 @@
-public interface IDebbugeable<T> where T : struct
-{
-    public T Data {get;}
-    public void Selected();
-    public void Deselected();
-    public void UpdateData(T data);
-    public void Deactivate();
-}
