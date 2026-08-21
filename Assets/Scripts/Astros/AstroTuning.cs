cc1d3709c45617a3f36fd23ebae2c851b3aa220f
elwatona <elwa.bazaesg@gmail.com>
2026-08-20 22:09:41 -0400

    refactor(tunning values): added way more values to the maximun tunning for astros

--- Assets/Scripts/Astros/AstroTuning.cs
+++ Assets/Scripts/Astros/AstroTuning.cs
@@ -1,25 +1,25 @@
 public static class AstroTuning
 {
     public const float OrbitRadiusMin = 1f;
-    public const float OrbitRadiusMax = 10f;
+    public const float OrbitRadiusMax = 999f;
 
     public const float GravityMin = 50f;
-    public const float GravityMax = 200f;
+    public const float GravityMax = 999f;
 
     public const float BodyRadiusMin = 0.5f;
-    public const float BodyRadiusMax = 7.5f;
+    public const float BodyRadiusMax = 99f;
 
     public const float OrbiterSpeedMin = 0.1f;
-    public const float OrbiterSpeedMax = 15f;
+    public const float OrbiterSpeedMax = 99f;
 
     public const float OrbiterRadiusMin = 0.5f;
-    public const float OrbiterRadiusMax = 20f;
+    public const float OrbiterRadiusMax = 99f;
 
     public const float OrbiterEccentricityMin = 0.01f;
     public const float OrbiterEccentricityMax = 0.97f;
 
     public const float RotationSpeedMin = 0.1f;
-    public const float RotationSpeedMax = 15f;
+    public const float RotationSpeedMax = 99f;
 
 #region Orbiter Path Settings
     public const float OrbiterPathMarginFactor = 0.5f;
